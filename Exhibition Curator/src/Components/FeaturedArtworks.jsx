ecff837937f65d5bde6aeb74a3adafa978c74179
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2025-01-15 20:44:14 +0000

    Update artwork component

--- Exhibition Curator/src/Components/FeaturedArtworks.jsx
+++ Exhibition Curator/src/Components/FeaturedArtworks.jsx
@@ -1,13 +1,47 @@
-import metGetRequests from "../Utilities/metMuseumApi"
+import metRequests from "../Utilities/metMuseumApi"
+import { useState } from "react"
+import { useEffect } from "react"
 
 function FeaturedArtworks() {
-    console.dir(metGetRequests) 
+
+    const [artworks, setArtworks] = useState([])
+
+    const getArtworks = (count = 6) => { 
+        return metRequests.getValidObjectNumbers()
+            .then((validIDs) => {
+                const randomIDs = [];
+                while (randomIDs.length < count) {
+                    const randomIndex = Math.floor(Math.random() * validIDs.length);
+                    const randomID = validIDs[randomIndex]
+                    randomIDs.push(randomID)
+                }
+                const artworkPromises = randomIDs.map((id) => { return metRequests.getObjectByID(id) })
+                return Promise.all(artworkPromises)
+            })
+            }
+    useEffect(() => { 
+        getArtworks(6).then((response) => {
+            setArtworks(response)
+        })
+    }, [])
+
+    console.log(artworks)
+    // need to filter the artworks so that they always have primaryImageSmall. Filter getValidIDs?
     return (
-      <div className="FeaturedArtworks">
-            <p>Hello, I am a FeaturedArtworks section</p>
-            
-      </div>
-    );
+        <div>
+            <ul>
+                {artworks.map((artwork) => {
+                    return (
+                        <li>
+                            
+                            <img src={artwork[primaryImageSmall]} alt="" />
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
 }
+
 
 export default FeaturedArtworks
