dece28cecd3f75f1fb4d86eed07c3b2ae3fa22de
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2025-01-16 21:35:06 +0000

    Renders Images!!!

--- Exhibition Curator/src/Components/FeaturedArtworks.jsx
+++ Exhibition Curator/src/Components/FeaturedArtworks.jsx
@@ -2,55 +2,6 @@ import metRequests from "../Utilities/metMuseumApi"
 import { useState } from "react"
 import { useEffect } from "react"
 
-// function FeaturedArtworks() {
-//   const [artworks, setArtworks] = useState([]);
-
-//   const getArtworks = (count = 6) => {
-//     return metRequests.getValidObjectNumbers().then((validIDs) => {
-//       const randomIDs = [];
-//       while (randomIDs.length < count) {
-//         const randomIndex = Math.floor(Math.random() * validIDs.length);
-//         const randomID = validIDs[randomIndex];
-//           metRequests.getObjectByID(randomID).then((artwork) => {
-//               if (artwork[primaryImageSmall]) {
-//                 randomIDs.push(randomID);
-//               }
-//           })
-//           continue
-//       }
-//       const artworkPromises = randomIDs.map((id) => {
-//         return metRequests.getObjectByID(id);
-//       });
-//       return Promise.all(artworkPromises);
-//     });
-//   };
-//   useEffect(() => {
-//     getArtworks(6).then((response) => {
-//       setArtworks(response);
-//     });
-//   }, []);
-
-//     console.log(artworks);
-
-  // need to filter the artworks so that they always have primaryImageSmall. Filter getValidIDs?
-//   return (
-//       <div>
-//           <p>Hello</p>
-//       <ul>
-//         {/* {artworks.map((artwork) => {
-//           return (
-//               <li>
-//                   <p>hello</p>
-//               <img src={artwork[primaryImageSmall]} alt="" />
-//             </li>
-//           ); */}
-//         {/* })} */}
-//       </ul>
-//     </div>
-//   );
-// }
-
-
 function FeaturedArtworks() {
 
     const [artworkIDs, setArtworkIDs] = useState([])
@@ -67,20 +18,32 @@ function FeaturedArtworks() {
     useEffect(() => {
         if (artworkIDs.length !== 0) {
             const fetchArtworks = async () => {
-                const artworkPromises = artworkIDs.map((ID) => {metRequests.getObjectByID(ID)})
+                const artworkPromises = artworkIDs.map((ID) => {
+                    return metRequests.getObjectByID(ID)
+                    
+                })
                 const artworkData = await Promise.all(artworkPromises);
+                
                 setArtworks(artworkData);
             }
             fetchArtworks()
-            console.log(artworks)
-            console.log(artworkIDs)
         }
     }, [artworkIDs]) 
-    
     return (
-        <>
-            <p>Hello</p>
-        </>
+        <div>
+            <ul>
+                {artworks.map((artwork) => {
+                    { console.log("hello") }
+                    return (
+                    <li key={artwork["objectID"]}>
+                      {artwork["primaryImageSmall"] && (
+                        <img src={artwork["primaryImageSmall"]} alt="Artwork" />
+                      )}
+                    </li> 
+                    )
+                })} 
+            </ul>
+        </div>
     )
 }
 
